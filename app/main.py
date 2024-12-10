5350db7d2c3e8c9d8a0d1828f736b2b146ecab8c
manojrajgopal <manojraj15@yahoo.com>
2024-12-11 00:01:03 +0530

    modified index.html

--- app/main.py
+++ app/main.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def main():
     return render_template('index.html', title='Outfit Recommender')  # Flask looks in 'templates' by default
 
+
 if __name__ == '__main__':
     app.run(debug=True)
